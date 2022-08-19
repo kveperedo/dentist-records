a888af5913d84c7006a2162994de8fea78429c4a
Kevin Peredo <kveperedo@gmail.com>
2022-08-20 00:13:48 +0800

    unfinished commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,10 @@
+import { PlusIcon } from '@radix-ui/react-icons';
 import type { NextPage } from 'next';
-import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { trpc } from '../utils/trpc';
-import { getProviders } from 'next-auth/react';
-
-type TechnologyCardProps = {
-	name: string;
-	description: string;
-	documentation: string;
-};
 
 const Home: NextPage = () => {
 	const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }]);
-	const { data } = trpc.useQuery(['question.getSession']);
-	getProviders().then((resp) => console.log(resp));
 
 	return (
 		<>
@@ -23,55 +14,20 @@ const Home: NextPage = () => {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<main className='container mx-auto flex flex-col items-center justify-center min-h-screen p-4'>
-				<h1 className='text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700'>
-					Create <span className='text-purple-300'>T3</span> App
-				</h1>
-				<p className='text-2xl text-gray-700'>This stack uses:</p>
-				<div className='grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3'>
-					<TechnologyCard
-						name='NextJS'
-						description='The React framework for production'
-						documentation='https://nextjs.org/'
-					/>
-					<TechnologyCard
-						name='TypeScript'
-						description='Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale'
-						documentation='https://www.typescriptlang.org/'
-					/>
-					<TechnologyCard
-						name='TailwindCSS'
-						description='Rapidly build modern websites without ever leaving your HTML'
-						documentation='https://tailwindcss.com/'
-					/>
-					<TechnologyCard
-						name='tRPC'
-						description='End-to-end typesafe APIs made easy'
-						documentation='https://trpc.io/'
-					/>
-				</div>
-				<div className='pt-6 text-2xl text-blue-500 flex justify-center items-center w-full'>
-					{hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
-				</div>
-			</main>
+			<div className='flex min-h-screen'>
+				<nav className='w-48 p-4 shadow'>Dental Records</nav>
+				<main className='flex flex-1 flex-col bg-slate-100 p-4'>
+					<div className='mb-4 flex items-center'>
+						<h1 className='text-xl font-bold'>Dental Records</h1>
+						<button className='ml-auto flex items-center gap-2 bg-violet-100 py-2 px-4 font-medium text-violet-600 transition-all hover:bg-violet-200 active:bg-violet-300'>
+							<PlusIcon className='h-4 w-4 text-base text-inherit' />
+							Add Record
+						</button>
+					</div>
+					<div className='flex-1 bg-white p-4'>Table</div>
+				</main>
+			</div>
 		</>
-	);
-};
-
-const TechnologyCard = ({ name, description, documentation }: TechnologyCardProps) => {
-	return (
-		<section className='flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105'>
-			<h2 className='text-lg text-gray-700'>{name}</h2>
-			<p className='text-sm text-gray-600'>{description}</p>
-			<a
-				className='mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2'
-				href={documentation}
-				target='_blank'
-				rel='noreferrer'
-			>
-				Documentation
-			</a>
-		</section>
 	);
 };
 
