39f75d1d6b848ff9d9cb5573cd81c4c57519d974
Kevin Peredo <kveperedo@gmail.com>
2022-08-27 17:45:22 +0800

    [feat] Add packages to use

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,34 @@ module.exports = {
 		fontFamily: {
 			poppins: ['Poppins', 'sans-serif'],
 		},
-		extend: {},
+		extend: {
+			animation: {
+				'fade-in': 'fade-in 0.25s',
+				'fade-out': 'fade-out 0.25s',
+			},
+			keyframes: {
+				'fade-in': {
+					'0%': {
+						opacity: 0,
+					},
+					'100%': {
+						opacity: 1,
+					},
+				},
+				'fade-out': {
+					'0%': {
+						opacity: 1,
+					},
+					'100%': {
+						opacity: 0,
+					},
+				},
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-radix')({
+			variantPrefix: 'rdx',
+		}),
+	],
 };
