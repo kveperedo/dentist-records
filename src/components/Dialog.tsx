208680202b14eacc033f6a0a28372dd44190593b
Kevin Peredo <kveperedo@gmail.com>
2022-08-29 12:51:53 +0800

    [chore] Update Dialog/Input component
    - Updated styling of Dialog and Input components

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { Cross1Icon } from '@radix-ui/react-icons';
+import { twMerge } from 'tailwind-merge';
 
 interface DialogContentProps {
 	children: React.ReactNode;
 	title: React.ReactNode;
+	className?: string;
 }
 
 export const DialogContent = React.forwardRef<HTMLDivElement, DialogContentProps>(
-	({ children, title, ...props }, forwardedRef) => (
+	({ children, title, className }, forwardedRef) => (
 		<DialogPrimitive.Portal>
 			<DialogPrimitive.Overlay className='rdx-state-open:animate-fade-in rdx-state-closed:animate-fade-out fixed inset-0 bg-black/40' />
 
 			<DialogPrimitive.Content
-				className='rdx-state-open:animate-fade-in rdx-state-closed:animate-fade-out fixed top-1/2 left-1/2 w-96 -translate-x-1/2 -translate-y-1/2 rounded bg-white p-8 shadow-sm'
-				{...props}
+				className={twMerge(
+					'rdx-state-open:animate-fade-in rdx-state-closed:animate-fade-out fixed top-1/2 left-1/2 w-96 -translate-x-1/2 -translate-y-1/2 rounded bg-white p-8 shadow-sm',
+					className
+				)}
 				ref={forwardedRef}
 			>
 				<DialogPrimitive.Title className='mb-8 text-xl font-medium text-slate-800'>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Label } from '@radix-ui/react-label';
+import * as LabelPrimitive from '@radix-ui/react-label';
 import { twMerge } from 'tailwind-merge';
 
 interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
 	startIcon?: React.ReactNode;
 	endIcon?: React.ReactNode;
-	label?: string;
 	id: string;
+	label?: string;
+	labelProps?: LabelPrimitive.LabelProps;
 	classes?: {
 		input?: string;
 		container?: string;
@@ -14,13 +15,17 @@ interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<H
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-	({ classes, label, startIcon, endIcon, id, ...props }, ref) => {
+	({ classes, label, labelProps, startIcon, endIcon, id, ...props }, ref) => {
 		return (
 			<div className={twMerge('flex flex-col gap-1', classes?.container)}>
 				{label && (
-					<Label className='text-sm text-slate-700' htmlFor={id}>
+					<LabelPrimitive.Label
+						className={twMerge('text-sm text-slate-700', labelProps?.className)}
+						htmlFor={id}
+						{...labelProps}
+					>
 						{label}
-					</Label>
+					</LabelPrimitive.Label>
 				)}
 				<div className='relative flex-1'>
 					{startIcon && <span className='absolute h-full w-10'>{startIcon}</span>}
@@ -29,7 +34,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 						id={id}
 						className={twMerge(
 							`w-full rounded border-2 border-slate-200 bg-white py-2 px-4
-                            text-slate-500 transition-colors focus:border-slate-600 focus-visible:outline-none`,
+                            text-slate-500 transition-colors hover:border-slate-400 focus:border-slate-600 focus-visible:outline-none`,
 							startIcon && 'pl-10',
 							endIcon && 'pr-10',
 							classes?.input
