f7aa8718f53713cd3daa141282f6d3a0fa574669
Kevin Peredo <kveperedo@gmail.com>
2022-08-29 17:03:30 +0800

    [feat] Update Record table
    - Removed age column in record table

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,13 +1,14 @@
 import { z } from 'zod';
 
+export const RecordStatus = z.enum(['single', 'married', 'divorced', 'separated', 'widowed']);
+
 export const Record = z.object({
 	id: z.string().optional(),
 	name: z.string(),
 	address: z.string(),
 	telephone: z.string(),
-	age: z.number().min(1),
 	occupation: z.string(),
-	status: z.enum(['single', 'married', 'divorced', 'separated', 'widowed']),
+	status: RecordStatus,
 	complaint: z.string(),
 	birthday: z.date(),
 });
