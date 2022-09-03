e8400a7565b8c9b172c52ff7a45b629fe7047c44
kveperedo <kveperedo@gmail.com>
2022-09-03 18:14:27 +0800

    [chore] Add readonly style to TextInput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -13,7 +13,7 @@ const TextInput = React.forwardRef<HTMLInputElement, TextInputProps & React.Comp
 			<MantineTextInput
 				unstyled
 				classNames={{
-                    root: twMerge(`flex flex-col gap-1`, classNames?.root),
+					root: twMerge(`flex flex-col gap-1`, classNames?.root),
 					label: twMerge(`text-sm text-slate-700`, classNames?.label),
 					required: twMerge(`text-slate-900`, classNames?.required),
 					wrapper: twMerge(`relative`, classNames?.wrapper),
@@ -23,6 +23,11 @@ const TextInput = React.forwardRef<HTMLInputElement, TextInputProps & React.Comp
                         font-poppins text-base text-slate-500 transition-colors 
                         hover:border-slate-400 focus:border-slate-600 focus-visible:outline-none
                         `,
+						props.readOnly &&
+							`
+						border-transparent border-b-slate-200 px-0 hover:border-transparent
+						hover:border-b-slate-400 focus:border-transparent focus:border-b-slate-600
+						`,
 						leftIcon && 'pl-10',
 						rightIcon && 'pr-10',
 						classNames?.input
